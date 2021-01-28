75b19cebe73fb8a0751d8a21fffac3745c9b774a
PauloRodriguesJr <pauloe_jr@hotmail.com>
2021-01-28 19:05:01 -0300

    Add alternative code with potentiometer

---
+++ MSD_transmitter_POT/MSD_transmitter_POT.ino
@@ -0,0 +1,66 @@
+#include <VirtualWire.h>
+#define pinPot A0
+#define pinRF  12
+#define pinLED 13
+#define pinLED_debug 11
+
+//int Buffer = 0; // para  dados recebidos na porta serial 
+
+struct Package{ // O pacote inclui os parâmetros necessarios para caracterizar o envio de dados
+  int  valor1 = 0;   //  Range 0- 1023
+  byte valor2 = 0    // no idea yet
+  int  valor3 = 0;   // Range 1000 -2000
+  };
+  
+Package pacote; 
+
+void setup() {
+  
+  vw_set_tx_pin(pinRF);      //Set the digital IO pin to use for transmit data. Defaults to 12.
+  
+  vw_set_ptt_inverted(true); //By default the PTT pin goes high when the transmitter is enabled. 
+                             //This flag forces it low when the transmitter is enabled. Required for the DR3100.                             
+  vw_setup(2000);            //Initialise the VirtualWire software, to operate at speed bits per second. 
+                             //Call this once in your setup() after any vw_set_* calls.
+                             //You must call vw_rx_start() after this before you will get any messages.
+  pinMode(pinLED, OUTPUT);
+  pinMode(pinLED_debug, OUTPUT);
+
+  Serial.begin(9600);  //Inicia monitor serial, onde serão escritos os dados
+  Serial.println("Iniciando... ");
+  delay(1500);
+  Serial.println("Aperte qualquer tecla para iniciar");
+  while(!Serial.available());  //Só continua quando vc mandar algo!
+  Serial.println("Envie dados entre 1000 e 2000 para o motor");//Sending messages for the user ,futuro: rotina de decolagem.
+  delay(1500);   //Sugestão -Add resposta em  % de feedback transmitido ao moço, só mudar serial  msg
+  Serial.println("Insira um valor de 1000 a 2000 de Thrust ");
+}
+
+void loop() {
+  digitalWrite(pinLED, HIGH);
+  //Le o valor do potenciometro
+  int val = analogRead(pinPot);
+  //Converte o valor para uma faixa entre 0 e 179
+  valor = map(valor, 0, 179, 0, 1023); //Observar os intervalos de operação, tinha colocado 1000 - 2000!
+  //Mostra o valor no serial monitor
+  Serial.print("Potentiometer Measurement: ");
+  Serial.println(val);
+  //Envia o valor para o motor
+
+  int  leitura1 = valor_179;   //Buffer; 
+  byte leitura2 = sizeof(leitura1);//map(leitura1, 0,179, 0, 1023); 
+  int  leitura3 = map(leitura1, 0, 1023, 1000, 2000); // Dado a ser usado
+
+  pacote.valor1 = leitura1;
+  pacote.valor2 = leitura2;
+  pacote.valor3 = leitura3;
+ 
+  Serial.println(pacote.valor1);
+  analogWrite(pinLED_debug, valor_179);
+  vw_send((uint8_t *)&pacote, sizeof(pacote));
+  vw_wait_tx(); 
+  
+  digitalWrite(pinLED, LOW);
+  delay(50);    
+
+}
